ddaa3318410c9d5bf022d0f3812a0a2e58bb9493
Hussain <hnkhandev@gmail.com>
2020-02-22 16:39:02 -0700

    Added beast class and listview builder

---
+++ lib/beast.dart
@@ -0,0 +1,13 @@
+class Beast {
+  final String name;
+  final String profile;
+  final List<String> reminders;
+  final String medicalHistory;
+
+  Beast({
+    this.name,
+    this.profile,
+    this.reminders,
+    this.medicalHistory,
+  });
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 
+import 'beast.dart';
+
 class HomePage extends StatelessWidget {
+  final List<Beast> beasts = [];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'My Beasts',
+          ),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: beasts.length,
+        itemBuilder: (context, index) {
+          return null;
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => null,
+      ),
+    );
   }
 }
